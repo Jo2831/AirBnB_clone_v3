a6983860535122d569433baabec5b9f6fc98feda
Jo2831 <Yosephman76@gmail.com>
2022-12-06 11:35:32 -0900

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+"""Python file that routes the Flask API's"""
 from api.v1.views import app_views
 from flask import jsonify
 
 
-
 @app_views.route('/status', methods=['GET'])
 def view_status():
+    """An end point to retrive ok status as a response"""
     return jsonify({"status": "OK"})
