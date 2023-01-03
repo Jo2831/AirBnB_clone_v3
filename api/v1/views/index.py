4ef0389270b36db8ae30a0bf2492597e0863fc88
Yoseph Tapano <yosephman76@gmail.com>
2023-01-03 16:33:33 +0300

    task3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """Python file that routes the Flask API's"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, request
+from models import storage
+
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
 
 
 @app_views.route('/status', methods=['GET'])
-def view_status():
+def show_status():
     """An end point to retrive ok status as a response"""
     return jsonify({"status": "OK"})
